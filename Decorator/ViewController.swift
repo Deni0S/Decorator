7b2f802ba4aff72993e7ace1a80ae6efc71bb4dc
Denis Barinov <DgamFX@mail.ru>
2020-05-30 21:29:03 +0300

    Initial Commit

---
+++ Decorator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Decorator
+//
+//  Created by Денис Баринов on 30.5.20.
+//  Copyright © 2020 Денис Баринов. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
